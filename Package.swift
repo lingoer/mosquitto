692d692a9b547bf6db2309e5bc61cc8976eeba38
Roy Fu <roy.fu@aylaasia.com>
2020-11-05 20:54:28 +0800

    chore: not using static flag

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Mosquitto", type: .static,
+            name: "Mosquitto",
             targets: ["Mosquitto"]),
     ],
     dependencies: [
